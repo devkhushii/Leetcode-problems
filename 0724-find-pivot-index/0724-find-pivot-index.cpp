1b1b7a87597b4ae1151a7a860ed2c7304d602a62
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-07-14 19:55:04 +0530

    Time: 0 ms (100%), Space: 35.7 MB (55.96%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
+    // int pivotIndex(vector<int>& nums) {
+    //     int n = nums.size();
+
+    //     vector<int> left(n);
+    //     vector<int> right(n);
+
+    //     left[0] = nums[0];
+    //     right[n-1] = nums[n-1];
+
+    //     // Left prefix
+    //     for(int i = 1; i < n; i++)
+    //         left[i] = left[i-1] + nums[i];
+
+    //     // Right prefix
+    //     for(int i = n-2; i >= 0; i--)
+    //         right[i] = right[i+1] + nums[i];
+
+    //     for(int i = 0; i < n; i++){
+
+    //         int leftSum = (i == 0) ? 0 : left[i-1];
+    //         int rightSum = (i == n-1) ? 0 : right[i+1];
+
+    //         if(leftSum == rightSum)
+    //             return i;
+    //     }
+
+    //     return -1;
+    // }
+
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
 
-        vector<int> left(n);
-        vector<int> right(n);
+        int total = 0;
 
-        left[0] = nums[0];
-        right[n-1] = nums[n-1];
+        for(int x : nums)
+            total += x;
 
-        // Left prefix
-        for(int i = 1; i < n; i++)
-            left[i] = left[i-1] + nums[i];
+        int leftSum = 0;
 
-        // Right prefix
-        for(int i = n-2; i >= 0; i--)
-            right[i] = right[i+1] + nums[i];
+        for(int i = 0; i < nums.size(); i++) {
 
-        for(int i = 0; i < n; i++){
-
-            int leftSum = (i == 0) ? 0 : left[i-1];
-            int rightSum = (i == n-1) ? 0 : right[i+1];
+            int rightSum = total - leftSum - nums[i];
 
             if(leftSum == rightSum)
                 return i;
+
+            leftSum += nums[i];
         }
 
         return -1;
