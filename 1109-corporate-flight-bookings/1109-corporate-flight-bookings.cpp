247ab80adea82d792d1f8943c6e3ac6816c5cff1
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-07-14 20:16:16 +0530

    Time: 27 ms (35.61%), Space: 78.5 MB (27.35%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> ans(n,0);
+    // vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+    //     vector<int> ans(n,0);
 
-        for(int i=0;i<bookings.size();i++){
-           int start=bookings[i][0];
-           int end=bookings[i][1];
-            for(int j=start;j<=end;j++){
-                ans[j-1]+=bookings[i][2];
-            }
+    //     for(int i=0;i<bookings.size();i++){
+    //        int start=bookings[i][0];
+    //        int end=bookings[i][1];
+    //         for(int j=start;j<=end;j++){
+    //             ans[j-1]+=bookings[i][2];
+    //         }
+    //     }
+    //     return ans;
+    // }
+     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+
+        vector<int> diff(n,0);
+
+        for(auto booking : bookings){
+
+            int first = booking[0];
+            int last = booking[1];
+            int seats = booking[2];
+
+            diff[first-1] += seats;
+
+            if(last < n)
+                diff[last] -= seats;
         }
-        return ans;
+
+        for(int i = 1; i < n; i++)
+            diff[i] += diff[i-1];
+
+        return diff;
     }
 };
