603eca0883620e12024ec0976725366693269a39
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-07-20 19:33:54 +0530

    Time: 0 ms (100%), Space: 55 MB (86.16%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
-
-        int i = 1;
-
-        while (true) {
-            if (s.find(i) == s.end()) {
-                return i;
+       int n = nums.size();
+        //cyclic sort
+        for(int i = 0; i < n; i++){
+            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]){
+                swap(nums[i] , nums[nums[i] - 1]);
             }
-            i++;
-        } 
+        }
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 };
