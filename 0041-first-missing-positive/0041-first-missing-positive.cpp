0f6a2f2b532d61ca9ab235f7ecca6924ee9ea8bf
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-07-20 19:34:53 +0530

    Time: 57 ms (16.22%), Space: 78.6 MB (5.1%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-       int n = nums.size();
-        //cyclic sort
-        for(int i = 0; i < n; i++){
-            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]){
-                swap(nums[i] , nums[nums[i] - 1]);
-            }
-        }
+    // int firstMissingPositive(vector<int>& nums) {
+    //    int n = nums.size();
+    //     //cyclic sort
+    //     for(int i = 0; i < n; i++){
+    //         while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]){
+    //             swap(nums[i] , nums[nums[i] - 1]);
+    //         }
+    //     }
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] != i + 1){
-                return i + 1;
+    //     for(int i = 0; i < n; i++){
+    //         if(nums[i] != i + 1){
+    //             return i + 1;
+    //         }
+    //     }
+    //     return n + 1;
+    // }
+
+       int firstMissingPositive(vector<int>& nums) {
+
+        unordered_set<int> s(nums.begin(), nums.end());
+
+        int i = 1;
+
+        while (true) {
+            if (s.find(i) == s.end()) {
+                return i;
             }
+            i++;
         }
-        return n + 1;
     }
 };
