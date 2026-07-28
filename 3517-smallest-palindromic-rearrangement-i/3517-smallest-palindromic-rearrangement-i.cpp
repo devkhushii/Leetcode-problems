fe0c3630773102399b9571de9ad84f33e27ac9e0
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-07-28 10:43:24 +0530

    Time: 5 ms (99.78%), Space: 54.3 MB (88.13%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,26 +1,53 @@
+// class Solution {
+// public:
+//     string smallestPalindrome(string s) {
+
+//         vector<int> freq(26, 0);
+
+//         for (char ch : s)
+//             freq[ch - 'a']++;
+
+//         string left = "";
+//         string mid = "";
+
+//         for (int i = 0; i < 26; i++) {
+
+//             left += string(freq[i] / 2, char('a' + i));
+
+//             if (freq[i] % 2 == 1)
+//                 mid = char('a' + i);
+//         }
+
+//         string right = left;
+//         reverse(right.begin(), right.end());
+
+//         return left + mid + right;
+//     }
+// };
 class Solution {
 public:
     string smallestPalindrome(string s) {
-
-        vector<int> freq(26, 0);
-
-        for (char ch : s)
-            freq[ch - 'a']++;
-
-        string left = "";
-        string mid = "";
-
-        for (int i = 0; i < 26; i++) {
-
-            left += string(freq[i] / 2, char('a' + i));
-
-            if (freq[i] % 2 == 1)
-                mid = char('a' + i);
+        int n = s.size();
+        int counts[26] = {0};
+        
+        // 1. O(N) 
+        for(int i = 0; i < n / 2; ++i) {
+            counts[s[i] - 'a']++;
         }
-
-        string right = left;
-        reverse(right.begin(), right.end());
-
-        return left + mid + right;
+        
+        // 2. O(N)
+        int idx = 0;
+        for(int i = 0; i < 26; ++i) {
+            while(counts[i]-- > 0) {
+                s[idx++] = (char)(i + 'a');
+            }
+        }
+        
+        // 3. O(N)
+        for(int i = 0; i < n / 2; ++i) {
+            s[n - 1 - i] = s[i];
+        }
+        
+        return s;
     }
 };
