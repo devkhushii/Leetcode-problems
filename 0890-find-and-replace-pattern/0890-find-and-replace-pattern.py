3a9a3a81439e41ce9950944676757729bfeb8bd7
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-08-14 22:42:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (35.74%) - LeetHub

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        sf = {}
+        tf = {}
+
+        if len(s) != len(t):
+            return False
+
+        for sch, tch in zip(s, t):
+
+            if sch in sf and sf[sch] != tch:
+                return False
+
+            if tch in tf and tf[tch] != sch:
+                return False
+
+            sf[sch] = tch
+            tf[tch] = sch
+
+        return True
+
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        result=[]
+        for word in words:
+            if self.isIsomorphic(word,pattern):
+                result.append(word)
+        return result
+        
