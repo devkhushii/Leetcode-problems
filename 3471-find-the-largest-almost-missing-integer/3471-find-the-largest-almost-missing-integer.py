b05e14d9daa8abd3bad6aabe989a47f55ee369f5
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-08-18 11:11:42 +0530

    Time: 3 ms (62.3%), Space: 19.2 MB (75.41%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -1,32 +1,45 @@
+# class Solution:
+#     def largestInteger(self, nums: List[int], k: int) -> int:
+#         n = len(nums)
+
+#         f = {}
+#         for num in nums:
+#             f[num] = f.get(num, 0) + 1
+
+#         # Every element forms its own window
+#         if k == 1:
+#             ans = -1
+
+#             for num in nums:
+#                 if f[num] == 1:
+#                     ans = max(ans, num)
+
+#             return ans
+
+#         # Only one window: the entire array
+#         if k == n:
+#             return max(nums)
+
+#         # 1 < k < n
+#         ans = -1
+
+#         if f[nums[0]] == 1:
+#             ans = max(ans, nums[0])
+
+#         if f[nums[-1]] == 1:
+#             ans = max(ans, nums[-1])
+
+#         return ans
+
 class Solution:
     def largestInteger(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-
-        f = {}
-        for num in nums:
-            f[num] = f.get(num, 0) + 1
-
-        # Every element forms its own window
-        if k == 1:
-            ans = -1
-
-            for num in nums:
-                if f[num] == 1:
-                    ans = max(ans, num)
-
-            return ans
-
-        # Only one window: the entire array
-        if k == n:
+        if k == len(nums):
             return max(nums)
 
-        # 1 < k < n
-        ans = -1
-
-        if f[nums[0]] == 1:
-            ans = max(ans, nums[0])
-
-        if f[nums[-1]] == 1:
-            ans = max(ans, nums[-1])
-
-        return ans
+        if k == 1:
+            arr = [x for x in nums if nums.count(x) == 1]
+        
+        else:
+            arr = [x for x in (nums[0], nums[-1]) if nums.count(x) == 1]
+        
+        return max(arr) if arr else -1
