a0ca025679d269f9d23b3349e61bf166bad809a6
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-08-23 00:46:57 +0530

    Time: 41 ms (82.5%), Space: 24 MB (6.17%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        n=len(s)
+        # if n%2==0:
+        #     left=(n//2)-1
+        #     right=n//2
+        # else:
+        #     left=(n//2)-1
+        #     right=(n//2)+1
+        # st=[]
+        def lps_compute(pattern):
+            m=len(pattern)
+            lps=[0]*m
+            length=0
+            i=1
+            while i<m:
+                if pattern[i]==pattern[length]:
+                    length+=1
+                    lps[i]=length
+                    i+=1
+                elif length!=0:
+                    length=lps[length-1]
+                else:
+                    lps[i]=0
+                    i+=1
+            return lps
+        rev=s[::-1]
+        combine=s+"#"+rev
+        lps=lps_compute(combine)  
+        prefix_len = lps[-1]
+        remaining=s[prefix_len:]
+        return remaining[::-1]+s     
