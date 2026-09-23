87945f5f959d34c0a0c8bbaf33f6428b936b385b
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-09-23 11:57:28 +0530

    Time: 0 ms (100%), Space: 19.5 MB (36.74%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permutation(self,nums, current, used, result):
+        if len(current)==len(nums):
+            result.append(current.copy())
+        
+        for num in nums:
+            if num not in used:
+                current.append(num)
+                used.add(num)
+                self.permutation(nums,current,used,result)
+
+                current.pop()
+                used.remove(num)
+
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        current=[]
+        result=[]
+        used=set()
+        self.permutation(nums, current, used, result)
+        return result
+        
